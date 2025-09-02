0a955ced0e3434acb7210b0f42aa41fa1beb6212
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2025-09-02 14:28:22 +0100

    add challenge 5 and 4

--- day 1/Challenges/les Boucles/main.c
+++ day 1/Challenges/les Boucles/main.c
@@ -24,13 +24,51 @@
 
 // Challenge 3
 
-int main(){
-     int number ;
-     int  somme = 0 ;
-     printf("inter your namber : ");
-     scanf("%d",&number);
-     for( int i = 0 ; i <= number ; i++){
-        somme = somme + i ;
-     }
-     printf("somme = %d ", somme);
-}
+// int main(){
+//      int number ;
+//      int  somme = 0 ;
+//      printf("inter your namber : ");
+//      scanf("%d",&number);
+//      for( int i = 0 ; i <= number ; i++){
+//         somme = somme + i ;
+//      }
+//      printf("somme = %d ", somme);
+// }
+
+//challenge 4 
+
+// int main(){
+//     int nPremiers = 0 ; 
+//     int nscane ;
+//     int i = 1 ;
+//     printf("inter your number ; ");
+//     scanf("%d",&nscane);
+//     while (nPremiers < nscane)
+//     {
+//        if (i%2 != 0)
+//        {
+//         printf("%d",i);
+//         nPremiers++ ;
+//        }
+//        i++ ;
+       
+//     }
+// }
+
+//challenge 5 
+
+// int main(){
+//     int number ;
+//     int puissance ;
+//     int resulte = 1 ;
+//     printf("inter le number ; ");
+//     scanf("%d",&number);
+//     printf("inter le puissance ; ");
+//     scanf("%d",&puissance);
+//     for (int i = 0; i < puissance; i++)
+//     {
+//         resulte = resulte * number ;
+//     }
+//     printf("%d",resulte);
+    
+// }
