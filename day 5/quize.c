798ab82d93ae211f7fcc8807b3bfdeac5493301b
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2025-09-08 16:05:06 +0100

    fin quize

--- day 5/quize.c
+++ day 5/quize.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
+int main()
+{
+    int input;
+    printf("ecrir number ; ");
+    scanf("%d", &input);
 
-int main(){
-    int input = 10 ;
-
-    for (int i = 0; i <= input*2; i++)
+    for (int i = 0; i <= input * 2; i++)
     {
-        for (int j = 0; j <= input*2; j++)
+        for (int j = 0; j <= input * 2; j++)
         {
-            if ( i + j == input || j - input == i ||  i - input == j || i + j == input*2+input  || j == input || i == input )
-            {
-                printf("*");
-            }else if (i < input && j < input && i+j>input)
-            {
-                printf("*");
-            }else if (i > input && j > input && i+j<input*3)
-            {
-                printf("*");
-            }else if (i>input && j<input && i - j < input)
-            {
-                printf("*");
-            }else if (i<input && j>input && j-i<input)
+            if (i + j == input || j - input == i || i - input == j || i + j == input * 2 + input || j == input || i == input)
             {
                 printf("*");
             }
-            
-            
+            else if (i < input && j < input && i + j > input)
+            {
+                printf("*");
+            }
+            else if (i > input && j > input && i + j < input * 3)
+            {
+                printf("*");
+            }
+            else if (i > input && j < input && i - j < input)
+            {
+                printf("*");
+            }
+            else if (i < input && j > input && j - i < input)
+            {
+                printf("*");
+            }
+
             else
             {
                 printf(" ");
@@ -34,5 +39,4 @@ int main(){
         }
         printf("\n");
     }
-    
 }
